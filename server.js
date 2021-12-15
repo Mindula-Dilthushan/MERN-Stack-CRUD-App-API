e1097904c868da4bf5ce1bfb3bb983040b3bc382
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-12-15 21:27:41 +0530

    comments

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+//Import Express
 const express = require('express');
 
+//Use Express
 const app = express()
 
+//Create port
 const port = 8000;
 
+//App Running
 app.listen(port,() =>{
     console.log(`App is running on ${port}`);
 });
